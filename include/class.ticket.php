f517a70a2cbc123655b75b4323c3479eb4fff201
Gabriel Majeri <gabriel.majeri6@gmail.com>
2022-01-14 09:35:17 +0200

    Remove inexistent function reference for 1.14.x

--- include/class.ticket.php
+++ include/class.ticket.php
@@ -2817,8 +2817,6 @@ implements RestrictedAccess, Threadable, Searchable {
                 $this->team_id = $assignee->getId();
                 $evd = array('team' => $assignee->getId());
                 $audit = array('team' => $assignee->getName());
-                if (($referral=$this->hasReferral($assignee,ObjectModel::OBJECT_TYPE_TEAM)))
-                    $referral->delete();
                 if ($this->getStaffId())
                     $this->staff_id = 0;
             }
