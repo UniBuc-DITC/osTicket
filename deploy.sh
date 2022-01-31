2145d844a9b744bad1e2e9068b3076d681ac5835
Gabriel Majeri <gabriel.majeri6@gmail.com>
2022-01-31 12:35:56 +0200

    Fix missing Apache server name

--- deploy.sh
+++ deploy.sh
@@ -36,6 +36,7 @@ ssh -T "root@$hostname" true
 # Read the environment variables from an env file
 echo "Reading machine env vars"
 env_vars=$(cat ".env.$env_vars_suffix" | xargs)
+env_vars="SERVER_NAME='$hostname' $env_vars"
 
 # If requested, configure firewall
 if [[ "$configure_firewall" = true ]]
